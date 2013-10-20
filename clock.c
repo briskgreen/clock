42a37c1aade06759a11d0b842758948988dc9893
briskgreen <briskgreen@163.com>
2013-10-21 00:20:34 +0800

    update

--- clock.c
+++ clock.c
@@ -5,20 +5,25 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <errno.h>
+
 void error_quit(char *msg)
 {
 	perror(msg);
 	exit(1);
 }
+
 void *normal_quit(void *win)
 {
 	while(getchar()!='q')
 		continue;
+
 	delwin((WINDOW *)win);
 	endwin();
+
 	echo();
 	printf("Exit. . .\n");
 	printf("\nExit Clock Now. . .\n");
+
 	exit(0);
 }
 int main(void)
@@ -28,18 +33,23 @@ int main(void)
 	char buf[30];
 	pthread_t thread;
 	WINDOW *win;
+
 	initscr();
 	curs_set(0);
 	noecho();
+
 	getmaxyx(stdscr,y,x);
 	if((win=newwin(5,40,y/2-5/2,x/2-40/2))==NULL)
 		error_quit("Create New Window Error!");
 	refresh();
+
 	box(win,0,0);
 	wrefresh(win);
 	getmaxyx(win,y,x);
+
 	if(pthread_create(&thread,NULL,normal_quit,win)==-1)
 		error_quit("Create thread Error!");
+
 	while(1)
 	{
 		t=time(NULL);
